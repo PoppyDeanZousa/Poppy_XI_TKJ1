cfae76de7363ebbe11832126d995fc010a2e26e0
poppy dean zousa <poppyzousa@gmail.com>
2023-11-25 22:57:47 +0700

    upload tugas

---
+++ fungsi/fungsi_praktek3.py
@@ -0,0 +1,8 @@
+misal e iki tugas 1 sampek 5 wes mok isi kan... ngko gantinen dewe isine jawaban e yaaaa
+
+
+klik menu sing enek ongkone terus klik + nek tulisan changes
+
+kan wes pindah dari changes ke staged changes
+
+terus isi pesan terus klik commit
